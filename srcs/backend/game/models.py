37b435d3797087b19074c70e65bb937cc676d17f
Thomas <none@none.fr>
2024-10-21 20:04:57 +0200

    Fix: user @me

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -1,40 +1,45 @@
 from django.db import models
 from authentication.models import User
 
-class MatchPlayer(models.Model):
+import uuid
 
+balls_direction = (
+    (1, "RIGHT_UP"),
+    (2, "RIGHT_DOWN"),
+    (3, "LEFT_UP"),
+    (4, "LEFT_DOWN"),
+)
+
+class MatchPlayer(models.Model):
     status_type = (
         (1, "ACTIVE"),
         (2, "INACTIVE"),
     )
-
     status = models.IntegerField(choices=status_type, default=1)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=10)
 
 class Match(models.Model):
     status_type = (
+        (1, "PENDING"),
         (2, "ACTIVE"),
         (3, "FINISHED"),
         (4, "CANCELLED"),
     )
 
-
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    # status = models.IntegerField(choices=status_type, default=1)
     player1 = models.ForeignKey(MatchPlayer, on_delete=models.CASCADE, related_name='player1')
-    player2 = models.ForeignKey(MatchPlayer, on_delete=models.CASCADE, related_name='player2')
-
+    player2 = models.ForeignKey(MatchPlayer, on_delete=models.CASCADE, related_name='player2', null=True, blank=True)
     player1_score = models.IntegerField()
     player2_score = models.IntegerField()
 
-    winner = models.ForeignKey(MatchPlayer, on_delete=models.CASCADE, related_name='winner')
+    winner = models.ForeignKey(MatchPlayer, on_delete=models.CASCADE, related_name='winner', null=True, blank=True)
     max_score = models.IntegerField()
-
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 
 class Tournament(models.Model):
     status_type = (
@@ -47,17 +52,39 @@ class Tournament(models.Model):
     name = models.CharField(max_length=255)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-
     is_active = models.BooleanField(default=True)
-
     max_players = models.IntegerField()
     max_score = models.IntegerField()
-
     match_duration = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     players = models.ManyToManyField(MatchPlayer, related_name='tournaments')
-
     chat = models.OneToOneField('chat.Channel', on_delete=models.CASCADE, null=True, blank=True)
 
+# Nouveau modèle pour les rounds
+class TournamentRound(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='rounds')
+    round_number = models.IntegerField()
+    status = models.IntegerField(choices=(
+        (1, "PENDING"),
+        (2, "IN_PROGRESS"),
+        (3, "COMPLETED")
+    ))
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = ('tournament', 'round_number')
+        ordering = ['round_number']
+
+class TournamentMatch(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='tournament_matches')
+    round = models.ForeignKey(TournamentRound, on_delete=models.CASCADE, related_name='matches')
+    match = models.OneToOneField(Match, on_delete=models.CASCADE)
+    match_order = models.IntegerField()
+
+    class Meta:
+        unique_together = ('round', 'match_order')
+        ordering = ['match_order']
--- srcs/backend/game/views.py
+++ srcs/backend/game/views.py
@@ -1,3 +1,331 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from authentication.views import TokenFromCookieAuthentication
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+from django.db.models import Q
+
+from .models import Match, MatchPlayer, balls_direction
+
+
+class CreateMatchView(APIView):
+    authentication_classes = [TokenFromCookieAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        match_player_1, created = MatchPlayer.objects.get_or_create(user=request.user)
+
+        if match_player_1.display_name is None:
+            match_player_1.display_name = request.user.publicuser.display_name
+        match_player_1.save()
+
+        existing_match = Match.objects.filter(Q(player1=match_player_1) | Q(player2=match_player_1))
+
+        # delete all exist
+
+        for match in existing_match:
+            match.delete()
+
+        return Response({
+            "uuid": match_player_1.uuid,
+        })
+
+        # if existing_match.exists():
+        #     return Response({
+        #         "uuid": existing_match.first().uuid,
+        #         "status": existing_match.first().status,
+        #         "player_1": existing_match.first().player1 if {
+        #             "uuid": existing_match.first().player1.uuid,
+        #             "display_name": existing_match.first().player1.display_name,
+        #             "user": {
+        #                 "uuid": existing_match.first().player1.user.uuid,
+        #                 "display_name": existing_match().player1.user.publicuser.display_name,
+        #                 "avatar": existing_match().player1.user.publicuser.avatar.url if existing_match().player1.user.publicuser.avatar else None,
+        #             },
+        #         } else None,
+        #         "player_2": existing_match.first().player2 if {
+        #             "uuid": existing_match.first().player2.uuid,
+        #             "display_name": existing_match.first().player2.display_name,
+        #             "user": {
+        #                 "uuid": existing_match.first().player2.user.uuid,
+        #                 "display_name": existing_match().player2.user.publicuser.display_name,
+        #                 "avatar": existing_match().player2.user.publicuser.avatar.url if existing_match().player2.user.publicuser.avatar else None,
+        #             },
+        #         } else None,
+        #         "player1_score": existing_match.first().player1_score,
+        #         "player2_score": existing_match.first().player2_score,
+        #         "winner": existing_match.first().winner if {
+        #             "uuid": existing_match.first().winner.uuid,
+        #             "display_name": existing_match.first().winner.display_name,
+        #             "user": {
+        #                 "uuid": existing_match.first().winner.user.uuid,
+        #                 "display_name": existing_match().winner.user.publicuser.display_name,
+        #                 "avatar": existing_match().winner.user.publicuser.avatar.url if existing_match().winner.user.publicuser.avatar else None,
+        #             },
+        #         } else None,
+        #         "max_score": existing_match.first().max_score,
+        #         "start_date": existing_match.first().start_date,
+        #         "end_date": existing_match.first().end_date,
+        #         "created_at": existing_match.first().created_at,
+        #         "updated_at": existing_match.first().updated_at,
+        #     })
+        #
+        # match = Match.objects.create(
+        #     status=1,
+        #     player1=match_player_1,
+        #     player1_score=0,
+        #     player2_score=0,
+        #     max_score=5,
+        # )
+        #
+        # match.save()
+        #
+        # return Response({
+        #     "uuid": match.uuid,
+        #     "status": match.status,
+        #     "player_1": {
+        #         "uuid": match.player1.uuid,
+        #         "display_name": match.player1.display_name,
+        #         "user": {
+        #             "uuid": match.player1.user.uuid,
+        #             "display_name": match.player1.user.publicuser.display_name,
+        #             "avatar": match.player1.user.publicuser.avatar.url if match.player1.user.publicuser.avatar else None,
+        #         },
+        #     },
+        #     "player_2": None,
+        #     "player1_score": match.player1_score,
+        #     "player2_score": match.player2_score,
+        #     "winner": None,
+        #     "max_score": match.max_score,
+        #     "start_date": match.start_date,
+        #     "end_date": match.end_date,
+        #     "created_at": match.created_at,
+        #     "updated_at": match.updated_at,
+        # })
+
+class GetMatchView(APIView):
+    authentication_classes = [TokenFromCookieAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, match_uuid):
+        if not match_uuid:
+            return Response({
+                "error": "Match not found",
+            }, status=status.HTTP_404_NOT_FOUND)
+        match = Match.objects.get(uuid=match_uuid)
+
+        return Response({
+            "uuid": match.uuid,
+            "status": match.status,
+            "player_1": {
+                "uuid": match.player1.uuid,
+                "display_name": match.player1.display_name,
+                "user": {
+                    "uuid": match.player1.user.uuid,
+                    "display_name": match.player1.user.publicuser.display_name,
+                    "avatar": match.player1.user.publicuser.avatar.url if match.player1.user.publicuser.avatar else None,
+                },
+            },
+            "player_2": match.player2 if {
+                "uuid": match.player2.uuid,
+                "display_name": match.player2.display_name,
+                "user": {
+                    "uuid": match.player2.user.uuid,
+                    "display_name": match.player2.user.publicuser.display_name,
+                    "avatar": match.player2.user.publicuser.avatar.url if match.player2.user.publicuser.avatar else None,
+                },
+            } else None,
+            "player1_score": match.player1_score,
+            "player2_score": match.player2_score,
+            "winner": match.winner if {
+                "uuid": match.winner.uuid,
+                "display_name": match.winner.display_name,
+                "user": {
+                    "uuid": match.winner.user.uuid,
+                    "display_name": match.winner.user.publicuser.display_name,
+                    "avatar": match.winner.user.publicuser.avatar.url if match.winner.user.publicuser.avatar else None,
+                },
+            } else None,
+            "max_score": match.max_score,
+            "start_date": match.start_date,
+            "end_date": match.end_date,
+            "created_at": match.created_at,
+            "updated_at": match.updated_at,
+        })
+
+class JoinMatchView(APIView):
+    def post(self):
+        match_uuid = request.data.get('match_uuid')
+        display_name = request.data.get('display_name')
+
+        if not match_uuid:
+            return Response({
+                "error": "Match not found",
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        if display_name is None:
+            display_name = request.user.publicuser.display_name
+
+        match = Match.objects.get(uuid=match_uuid)
+
+        if match.status != 1:
+            return Response({
+                "error": "Match is not available",
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        match_player_2 = MatchPlayer.objects.get_or_create(user=request.user)
+        match_player_2.display_name = display_name
+        match_player_2.save()
+
+        match.player2 = match_player_2
+
+        match.status = 2
+        match.start_time = timezone.now()
+        match.save()
+
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            f"user_{str(match_player_1.user.uuid)}",
+            {
+                "type": "send_event",
+                "event_name": "GAME_START_MATCH",
+                "data": {
+                    "uuid": match.uuid,
+                    "status": match.status,
+                    "game": {
+                        "ball_direction": random.choice(balls_direction),
+                    },
+                    "player_1": {
+                        "uuid": match.player1.uuid,
+                        "display_name": match.player1.display_name,
+                        "user": {
+                            "uuid": match.player1.user.uuid,
+                            "display_name": match.player1.user.publicuser.display_name,
+                            "avatar": match.player1.user.publicuser.avatar.url if match.player1.user.publicuser.avatar else None,
+                        },
+                    },
+                    "player_2": {
+                        "uuid": match.player2.uuid,
+                        "display_name": match.player2.display_name,
+                        "user": {
+                            "uuid": match.player2.user.uuid,
+                            "display_name": match.player2.user.publicuser.display_name,
+                            "avatar": match.player2.user.publicuser.avatar.url if match.player2.user.publicuser.avatar else None,
+                        },
+                    },
+                    "player1_score": match.player1_score,
+                    "player2_score": match.player2_score,
+                    "winner": match.winner if {
+                        "uuid": match.winner.uuid,
+                        "display_name": match.winner.display_name,
+                        "user": {
+                            "uuid": match.winner.user.uuid,
+                            "display_name": match.winner.user.publicuser.display_name,
+                            "avatar": match.winner.user.publicuser.avatar.url if match.winner.user.publicuser.avatar else None,
+                        },
+                    } else None,
+                    "max_score": match.max_score,
+                    "start_date": match.start_date,
+                    "end_date": match.end_date,
+                    "created_at": match.created_at,
+                    "updated_at": match.updated_at,
+                }
+            }
+        )
+        async_to_sync(channel_layer.group_send)(
+            f"user_{str(match_player_2.user.uuid)}",
+            {
+                "type": "send_event",
+                "event_name": "GAME_START_MATCH",
+                "data": {
+                    "uuid": match.uuid,
+                    "status": match.status,
+                    "game": {
+                        "ball_direction": random.choice(balls_direction),
+                    },
+                    "player_1": {
+                        "uuid": match.player1.uuid,
+                        "display_name": match.player1.display_name,
+                        "user": {
+                            "uuid": match.player1.user.uuid,
+                            "display_name": match.player1.user.publicuser.display_name,
+                            "avatar": match.player1.user.publicuser.avatar.url if match.player1.user.publicuser.avatar else None,
+                        },
+                    },
+                    "player_2": {
+                        "uuid": match.player2.uuid,
+                        "display_name": match.player2.display_name,
+                        "user": {
+                            "uuid": match.player2.user.uuid,
+                            "display_name": match.player2.user.publicuser.display_name,
+                            "avatar": match.player2.user.publicuser.avatar.url if match.player2.user.publicuser.avatar else None,
+                        },
+                    },
+                    "player1_score": match.player1_score,
+                    "player2_score": match.player2_score,
+                    "winner": match.winner if {
+                        "uuid": match.winner.uuid,
+                        "display_name": match.winner.display_name,
+                        "user": {
+                            "uuid": match.winner.user.uuid,
+                            "display_name": match.winner.user.publicuser.display_name,
+                            "avatar": match.winner.user.publicuser.avatar.url if match.winner.user.publicuser.avatar else None,
+                        },
+                    } else None,
+                    "max_score": match.max_score,
+                    "start_date": match.start_date,
+                    "end_date": match.end_date,
+                    "created_at": match.created_at,
+                    "updated_at": match.updated_at,
+                }
+            }
+        )
+
+        return Response({
+            "uuid": match.uuid,
+            "status": match.status,
+            "player_1": {
+                "uuid": match.player1.uuid,
+                "display_name": match.player1.display_name,
+                "user": {
+                    "uuid": match.player1.user.uuid,
+                    "display_name": match.player1.user.publicuser.display_name,
+                    "avatar": match.player1.user.publicuser.avatar.url if match.player1.user.publicuser.avatar else None,
+                },
+            },
+            "player_2": {
+                "uuid": match.player2.uuid,
+                "display_name": match.player2.display_name,
+                "user": {
+                    "uuid": match.player2.user.uuid,
+                    "display_name": match.player2.user.publicuser.display_name,
+                    "avatar": match.player2.user.publicuser.avatar.url if match.player2.user.publicuser.avatar else None,
+                },
+            },
+            "player1_score": match.player1_score,
+            "player2_score": match.player2_score,
+            "winner": match.winner if {
+                "uuid": match.winner.uuid,
+                "display_name": match.winner.display_name,
+                "user": {
+                    "uuid": match.winner.user.uuid,
+                    "display_name": match.winner.user.publicuser.display_name,
+                    "avatar": match.winner.user.publicuser.avatar.url if match.winner.user.publicuser.avatar else None,
+                },
+            } else None,
+            "max_score": match.max_score,
+            "start_date": match.start_date,
+            "end_date": match.end_date,
+            "created_at": match.created_at,
+            "updated_at": match.updated_at,
+        })
+
+
+
+
