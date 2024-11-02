4a811f4690dea36ec193879ce32a80587514a980
Okan Koca <oknakoca@gmail.com>
2024-11-02 20:18:36 +0100

    added small pie chart to dashboard

--- srcs/frontend/srcs/js/dashboard.js
+++ srcs/frontend/srcs/js/dashboard.js
@@ -1,5 +1,6 @@
+import { eventEmitter } from "./eventemitter.js";
 import { BASE_URL } from "./handler.js";
-import { getCurrentUser } from "./storage.js";
+import { getCurrentUser, isDarkMode } from "./storage.js";
 
 const winrate = (user, gameData) => {
 	const gamesWon = (() =>
@@ -15,6 +16,51 @@ const winrate = (user, gameData) => {
 	const gamesDraw = (() => gameData.filter((game) => !game.winner))();
 
 	return { gamesWon, gamesLost, gamesDraw };
+};
+
+const drawPieChart = (
+	canvasId,
+	data = [{ length: 0, name: "placeholder" }]
+) => {
+	/**
+	 * @type {HTMLCanvasElement}
+	 */
+	// @ts-ignore
+	const canvas = document.getElementById(canvasId);
+	const ctx = canvas.getContext("2d");
+	const colors = ["#FF5733", "#33FF57", "#3357FF"];
+
+	const total = data.reduce((prev, curr) => prev + curr.length, 0);
+
+	let startAngle = 0;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	const radius = canvas.width / 6; // Adjust the radius to make the pie smaller
+	const centerX = canvas.width / 2;
+	const centerY = canvas.height / 2;
+
+	data.forEach(({ length, name }, index) => {
+		const sliceAngle = (length / total) * 2 * Math.PI;
+		const sliceMidAngle = startAngle + sliceAngle / 2;
+
+		ctx.beginPath();
+		ctx.moveTo(centerX, centerY);
+		ctx.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
+		ctx.closePath();
+		ctx.fillStyle = colors[index % colors.length];
+		ctx.fill();
+
+		const textX = centerX + (radius + 20) * Math.cos(sliceMidAngle);
+		const textY = centerY + (radius + 20) * Math.sin(sliceMidAngle);
+		ctx.fillStyle = isDarkMode() ? "white" : "black";
+		ctx.font = "14px Arial";
+		ctx.fillText(
+			`${name.toUpperCase()} ${((length / total) * 100).toFixed(1)}%`,
+			textX,
+			textY
+		);
+
+		startAngle += sliceAngle;
+	});
 };
 
 export const dashboardHandler = async () => {
@@ -46,4 +92,23 @@ export const dashboardHandler = async () => {
 		" Games Draw: ",
 		gamesDraw
 	);
+
+	const pieChartData = [
+		{
+			length: gamesWon.length,
+			name: "won",
+		},
+		{
+			length: gamesLost.length,
+			name: "lost",
+		},
+		{
+			length: gamesDraw.length,
+			name: "draw",
+		},
+	];
+
+	eventEmitter.on("theme", () => drawPieChart("wins-chart", pieChartData));
+
+	drawPieChart("wins-chart", pieChartData);
 };
