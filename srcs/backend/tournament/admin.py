e55ba7ef81cf32d313e48cacfc559006e3c8c6a9
Thomas <none@none.fr>
2024-11-04 17:51:03 +0100

    Add: Tournament Cancel In Reboot

--- srcs/backend/tournament/admin.py
+++ srcs/backend/tournament/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
-# Register your models here.
+
+class TournamentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tournament'
+    def ready(self):
+        from .models import Tournament
+
+        def cancel_active_and_pending_tournaments(sender, **kwargs):
+            Tournament.objects.filter(status__in=[1, 2]).update(status=4)  # 1 = PENDING, 2 = ACTIVE, 4 = CANCELLED
+
+        post_migrate.connect(cancel_active_and_pending_tournaments, sender=self)
